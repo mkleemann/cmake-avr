958179e04100170e0d6a3f6a71dd9135ac19100e
Daniel Otero <otero.o.daniel@gmail.com>
2015-04-19 21:17:09 +0200

    Renamed the CMAKE_SYSTEM_NAME from 'generic' to 'Generic'

--- generic-gcc-avr.cmake
+++ generic-gcc-avr.cmake
@@ -45,7 +45,7 @@ find_program(AVR_OBJDUMP avr-objdump)
 ##########################################################################
 # toolchain starts with defining mandatory variables
 ##########################################################################
-set(CMAKE_SYSTEM_NAME generic)
+set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR avr)
 set(CMAKE_C_COMPILER ${AVR_CC})
 set(CMAKE_CXX_COMPILER ${AVR_CXX})
