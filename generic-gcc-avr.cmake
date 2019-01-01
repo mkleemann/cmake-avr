a7be0379cfa6f1b571e3423a8c88b5010d6f07cf
Klaus Schneider-Zapp <klaus_snd@web.de>
2019-01-01 20:35:23 +0100

    Move general targets outside add_avr_executable

--- generic-gcc-avr.cmake
+++ generic-gcc-avr.cmake
@@ -230,55 +230,14 @@ function(add_avr_executable EXECUTABLE_NAME)
       COMMENT "Uploading ${eeprom_image} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
    )
 
-   # get status
-   add_custom_target(
-      get_status
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n -v
-      COMMENT "Get status from ${AVR_MCU}"
-   )
-
-   # get fuses
-   add_custom_target(
-      get_fuses
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n
-         -U lfuse:r:-:b
-         -U hfuse:r:-:b
-      COMMENT "Get fuses from ${AVR_MCU}"
-   )
-
-   # set fuses
-   add_custom_target(
-      set_fuses
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U lfuse:w:${AVR_L_FUSE}:m
-         -U hfuse:w:${AVR_H_FUSE}:m
-         COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
-   )
-
-   # get oscillator calibration
-   add_custom_target(
-      get_calibration
-         ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:r:${AVR_MCU}_calib.tmp:r
-         COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
-   )
-
-   # set oscillator calibration
-   add_custom_target(
-      set_calibration
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:w:${AVR_MCU}_calib.hex
-         COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
-   )
-
    # disassemble
    add_custom_target(
       disassemble_${EXECUTABLE_NAME}
       ${AVR_OBJDUMP} -h -S ${elf_file} > ${EXECUTABLE_NAME}.lst
       DEPENDS ${elf_file}
    )
-
 endfunction(add_avr_executable)
+
 
 ##########################################################################
 # add_avr_library
@@ -381,3 +340,44 @@ function(avr_target_compile_definitions EXECUTABLE_TARGET)
 
    target_compile_definitions(${TARGET_LIST} ${extra_args})
 endfunction()
+
+# get status
+add_custom_target(
+   get_status
+   ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n -v
+   COMMENT "Get status from ${AVR_MCU}"
+)
+
+# get fuses
+add_custom_target(
+   get_fuses
+   ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n
+      -U lfuse:r:-:b
+      -U hfuse:r:-:b
+   COMMENT "Get fuses from ${AVR_MCU}"
+)
+
+# set fuses
+add_custom_target(
+   set_fuses
+   ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
+      -U lfuse:w:${AVR_L_FUSE}:m
+      -U hfuse:w:${AVR_H_FUSE}:m
+      COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
+)
+
+# get oscillator calibration
+add_custom_target(
+   get_calibration
+      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
+      -U calibration:r:${AVR_MCU}_calib.tmp:r
+      COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
+)
+
+# set oscillator calibration
+add_custom_target(
+   set_calibration
+   ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
+      -U calibration:w:${AVR_MCU}_calib.hex
+      COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
+)
